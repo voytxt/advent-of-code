a73b0b17cc35c3317809cc68d53a1086caa6a5fd
Vojta <65811166+voytxt@users.noreply.github.com>
2023-12-01 18:49:27 +0100

    2023 day 1

--- 2023/1-2.ts
+++ 2023/1-2.ts
@@ -1,3 +1,5 @@
+const nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
 export default function main(input: string): string {
   let sum = 0;
 
@@ -5,13 +7,23 @@ export default function main(input: string): string {
     let firstDigit: number | null = null;
     let lastDigit: number | null = null;
 
-    for (const char of line) {
+    for (const [i, char] of Object.entries([...line])) {
       if (parseInt(char)) {
         if (firstDigit === null) {
           firstDigit = parseInt(char);
         }
 
         lastDigit = parseInt(char);
+      } else {
+        for (const [j, num] of Object.entries(nums)) {
+          if (line.slice(+i).startsWith(num)) {
+            if (firstDigit === null) {
+              firstDigit = +j + 1;
+            }
+
+            lastDigit = +j + 1;
+          }
+        }
       }
     }
 
